170ff926eff85cf64b6e98b8f1aa80f92569d4fd
huxinmin <744019705@qq.com>
2017-03-28 17:29:47 +0800

    siginin

--- expressMongo/models/users.js
+++ expressMongo/models/users.js
@@ -1,4 +1,10 @@
 var userSchema = require('../lib/mongoose').userSchema;
 var mongoose = require('mongoose');
 
-exports.userModel =  mongoose.model('user',userSchema);
+exports.userModel = mongoose.model('user', userSchema);
+
+module.getUserByName = function(name) {
+    return userModel
+        .findOne({ name: name })
+        
+}
