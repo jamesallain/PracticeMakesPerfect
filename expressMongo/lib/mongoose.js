9c0f003f2db818619ddfb2af5d5e58cbe122e73c
huxinmin <744019705@qq.com>
2017-03-29 10:17:32 +0800

    signin finish

--- expressMongo/lib/mongoose.js
+++ expressMongo/lib/mongoose.js
@@ -1,5 +1,8 @@
 var config = require('config-lite');
 var mongoose = require('mongoose');
+var moment = require('moment');
+var objectIdToTimestamp = require('objectid-to-timestamp');
+
 mongoose.connect(config.mongodb);
 
 exports.userSchema = new mongoose.Schema({
@@ -9,3 +12,12 @@ exports.userSchema = new mongoose.Schema({
     gender: { type: String, enum: ['m', 'f', 'x'] },
     bio: { type: String }
 });
+
+exports.addCreatedAt = function(schema) {
+	schema.add({ lastMod: Date })
+    schema.pre('findOne', function(next) {
+        this.lastMod = new Date;
+        next()
+    })
+}
+exports.userSchema.plugin(exports.addCreatedAt);
--- expressMongo/models/users.js
+++ expressMongo/models/users.js
@@ -2,9 +2,3 @@ var userSchema = require('../lib/mongoose').userSchema;
 var mongoose = require('mongoose');
 
 exports.userModel = mongoose.model('user', userSchema);
-
-module.getUserByName = function(name) {
-    return userModel
-        .findOne({ name: name })
-        
-}
